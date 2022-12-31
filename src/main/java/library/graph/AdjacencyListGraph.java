3eefab195b39672018e2bc88eda9842636ce3f78
Samuel Mwangi <mwangi.s.irungu@gmail.com>
2022-12-31 15:35:52 +0300

    Graph challenges

--- src/main/java/library/graph/AdjacencyListGraph.java
+++ src/main/java/library/graph/AdjacencyListGraph.java
@@ -1,6 +1,9 @@
 package library.graph;
 
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class AdjacencyListGraph {
     private final int vertices;
@@ -17,26 +20,116 @@ public class AdjacencyListGraph {
         }
     }
 
-    public void addEdge(int i, int j){
-        LinkedList<Integer> listI =  graph[i];
-        LinkedList<Integer> listJ =  graph[j];
-
+    public void addEdge(int i, int j) {
+        LinkedList<Integer> listI = graph[i];
         listI.addLast(j);
-        listJ.addLast(i);
     }
 
-    public void removeEdge(int i, int j){
-        LinkedList<Integer> listI =  graph[i];
-        LinkedList<Integer> listJ =  graph[j];
-
+    public void removeEdge(int i, int j) {
+        LinkedList<Integer> listI = graph[i];
         listI.removeFirstOccurrence(j);
-        listJ.removeFirstOccurrence(i);
     }
 
-    public String toString(){
+    public String bfsTraversal() {
+        HashSet<Integer> result = new HashSet<>();
         StringBuilder s = new StringBuilder();
 
-        for(int i = 0; i < vertices; i++){
+        if (vertices < 1) return "";
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.offer(0);
+        result.add(0);
+
+        while (queue.peek() != null) {
+            int item = queue.poll();
+            s.append(item).append("->");
+            for (int neighbor : graph[item]) {
+                if (!result.contains(neighbor)) {
+                    queue.offer(neighbor);
+                    result.add(neighbor);
+                }
+            }
+        }
+
+        return s.toString();
+    }
+
+    public String dfsTraversal() {
+        HashSet<Integer> result = new HashSet<>();
+        StringBuilder s = new StringBuilder();
+
+        if (vertices < 1) return "";
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        result.add(0);
+
+        while (!stack.empty()) {
+            int item = stack.pop();
+            s.append(item).append("->");
+            for (int neighbor : graph[item]) {
+                if (!result.contains(neighbor)) {
+                    stack.push(neighbor);
+                    result.add(neighbor);
+                }
+            }
+        }
+
+
+        return s.toString();
+    }
+
+    public void dfsTraversalRecursive(StringBuilder s, HashSet<Integer> items, int node) {
+        s.append(node).append("->");
+        for (int neighbor : graph[node]) {
+            if (!items.contains(neighbor)) {
+                items.add(neighbor);
+                dfsTraversalRecursive(s, items, neighbor);
+            }
+        }
+    }
+
+    public boolean detectCycle() {
+        Stack<Integer> stack = new Stack<>();
+        HashSet<Integer> tracker = new HashSet<>();
+
+        stack.push(0);
+        tracker.add(0);
+
+        while (!stack.empty()) {
+            int item = stack.pop();
+            for (int neighbor : graph[item]) {
+                if (tracker.contains(neighbor)) {
+                    return true;
+                }
+                tracker.add(neighbor);
+                stack.push(neighbor);
+            }
+
+        }
+
+        return false;
+    }
+
+    public boolean detectCycleRecursive(int node, HashSet<Integer> tracker) {
+
+        for (int neighbor : graph[node]) {
+            if (tracker.contains(neighbor)) {
+                return true;
+            }
+
+            tracker.add(neighbor);
+            return detectCycleRecursive(neighbor, tracker);
+        }
+        return false;
+
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+
+        for (int i = 0; i < vertices; i++) {
             s.append(i).append("->");
             for (int j : graph[i]) {
                 s.append(j);
@@ -46,4 +139,6 @@ public class AdjacencyListGraph {
 
         return s.toString();
     }
+
+
 }
--- src/test/java/library/graph/AdjacencyListGraphTest.java
+++ src/test/java/library/graph/AdjacencyListGraphTest.java
@@ -3,12 +3,15 @@ package library.graph;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
+
 public class AdjacencyListGraphTest {
     @Test
     public void testAddEdge(){
         int vertices =  2;
         AdjacencyListGraph graph =  new AdjacencyListGraph(vertices);
         graph.addEdge(0,1);
+        graph.addEdge(1, 0);
 
         String expectedGraph =  "0->1\n1->0\n";
 
@@ -21,11 +24,93 @@ public class AdjacencyListGraphTest {
         int vertices =  3;
         AdjacencyListGraph graph =  new AdjacencyListGraph(vertices);
         graph.addEdge(0,1);
+        graph.addEdge(1, 0);
         graph.addEdge(0,2);
+        graph.addEdge(2,0);
         graph.removeEdge(0,1);
+        graph.removeEdge(1,0);
 
         String expectedGraph =  "0->2\n1->\n2->0\n";
 
         Assert.assertEquals(graph.toString(),expectedGraph);
+    }
+
+    @Test
+    public void testBFSTraversal(){
+        int vertices = 4;
+        AdjacencyListGraph graph =  new AdjacencyListGraph(vertices);
+        graph.addEdge(0,1);
+        graph.addEdge(0,2);
+        graph.addEdge(1,3);
+        graph.addEdge(2, 3);
+
+        String expected = "0->1->2->3->";
+
+        Assert.assertEquals(graph.bfsTraversal(), expected);
+    }
+
+
+    @Test
+    public void testDFSTraversal(){
+        int vertices = 4;
+        AdjacencyListGraph graph =  new AdjacencyListGraph(vertices);
+        graph.addEdge(0,1);
+        graph.addEdge(0,2);
+        graph.addEdge(1,3);
+        graph.addEdge(2, 3);
+
+        String expected = "0->2->3->1->";
+
+        Assert.assertEquals(graph.dfsTraversal(), expected);
+    }
+
+    @Test
+    public void testDFSTraversalRecursive(){
+        int vertices = 4;
+        AdjacencyListGraph graph =  new AdjacencyListGraph(vertices);
+        graph.addEdge(0,1);
+        graph.addEdge(0,2);
+        graph.addEdge(1,3);
+        graph.addEdge(2, 3);
+
+        StringBuilder s = new StringBuilder();
+        HashSet<Integer> items =  new HashSet<>();
+
+        graph.dfsTraversalRecursive(s, items,0);
+
+        String expected = "0->1->3->2->";
+
+        Assert.assertEquals(s.toString(), expected);
+    }
+
+
+    @Test
+    public void testDetectCycle(){
+        AdjacencyListGraph graph =  new AdjacencyListGraph(2);
+        graph.addEdge(0,1);
+        graph.addEdge(1,0);
+
+        Assert.assertTrue(graph.detectCycle());
+
+        graph.removeEdge(1,0);
+        Assert.assertFalse(graph.detectCycle());
+
+        graph.removeEdge(0,1);
+        Assert.assertFalse(graph.detectCycle());
+    }
+
+    @Test
+    public void testDetectCycleRecursive(){
+        AdjacencyListGraph graph =  new AdjacencyListGraph(2);
+        graph.addEdge(0,1);
+        graph.addEdge(1,0);
+
+        Assert.assertTrue(graph.detectCycleRecursive(0, new HashSet<>()));
+
+        graph.removeEdge(1,0);
+        Assert.assertFalse(graph.detectCycleRecursive(0, new HashSet<>()));
+
+        graph.removeEdge(0,1);
+        Assert.assertFalse(graph.detectCycleRecursive(0, new HashSet<>()));
     }
 }
