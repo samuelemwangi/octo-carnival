86363e68d22cf6354324565737b7bea1d8376210
Samuel Mwangi <mwangi.s.irungu@gmail.com>
2023-01-08 00:09:12 +0300

    Tree sum, path root to leaf and minimum

--- src/main/java/library/tree/GeneralTreeChallenges.java
+++ src/main/java/library/tree/GeneralTreeChallenges.java
@@ -124,4 +124,69 @@ public class GeneralTreeChallenges<T extends Comparable<T>> {
 
         return searchRecursive(currNode.leftChild, value) || searchRecursive(currNode.rightChild, value);
     }
+
+    /***
+     *
+     * @param currNode current node
+     * @return sum of all nodes
+     * @complexity Time -> O(n), Space -> O(n)
+     */
+    public int getNodesSumRecursive(Node<Integer> currNode) {
+        if (currNode == null)
+            return 0;
+
+        return currNode.data +
+                getNodesSumRecursive(currNode.leftChild) +
+                getNodesSumRecursive(currNode.rightChild);
+    }
+
+    /***
+     *
+     * @param currNode current node
+     * @return sum of all nodes
+     * @complexity Time -> O(n), Space -> O(n)
+     */
+    public int findMinimumValueRecursive(Node<Integer> currNode) {
+        if (currNode == null)
+            return Integer.MAX_VALUE;
+
+        int minLeft = findMinimumValueRecursive(currNode.leftChild);
+        int minRight = findMinimumValueRecursive(currNode.rightChild);
+
+        return Math.min(currNode.data, Math.min(minLeft, minRight));
+    }
+
+    public int findMinimumValueIterative(Node<Integer> currNode) {
+        int minValue = Integer.MAX_VALUE;
+        if (currNode == null)
+            return minValue;
+
+        Queue<Node<Integer>> queue = new ArrayDeque<>();
+        queue.offer(currNode);
+
+        while (queue.peek() != null) {
+            Node<Integer> item = queue.poll();
+            minValue = Math.min(minValue, item.data);
+
+            if (item.leftChild != null)
+                queue.offer(item.leftChild);
+
+            if (item.rightChild != null)
+                queue.offer(item.rightChild);
+        }
+
+        return minValue;
+    }
+
+    public int maxRootToLeafPathSumRecursive(Node<Integer> currNode){
+        if(currNode == null)
+            return Integer.MIN_VALUE;
+        if(currNode.leftChild == null && currNode.rightChild == null)
+            return currNode.data;
+
+        int leftValue =  maxRootToLeafPathSumRecursive(currNode.leftChild);
+        int rightValue = maxRootToLeafPathSumRecursive(currNode.rightChild);
+
+        return  currNode.data + Math.max(leftValue, rightValue);
+    }
 }
--- src/test/java/library/tree/GeneralTreeChallengesTest.java
+++ src/test/java/library/tree/GeneralTreeChallengesTest.java
@@ -20,16 +20,16 @@ public class GeneralTreeChallengesTest {
         b.leftChild = d;
         b.rightChild = e;
 
-        c.rightChild =  f;
+        c.rightChild = f;
 
-        GeneralTreeChallenges<Character> challenges =  new GeneralTreeChallenges<>();
-        String expected =  "a,b,d,e,c,f,";
+        GeneralTreeChallenges<Character> challenges = new GeneralTreeChallenges<>();
+        String expected = "a,b,d,e,c,f,";
         Assert.assertEquals(challenges.dfsTraversalIterative(root), expected);
         Assert.assertEquals(challenges.dfsTraversalRecursive(root), expected);
     }
 
     @Test
-    public void testBFSTraversal(){
+    public void testBFSTraversal() {
         Node<Character> root = new Node<>('a'),
                 b = new Node<>('b'),
                 c = new Node<>('c'),
@@ -43,15 +43,15 @@ public class GeneralTreeChallengesTest {
         b.leftChild = d;
         b.rightChild = e;
 
-        c.rightChild =  f;
+        c.rightChild = f;
 
-        GeneralTreeChallenges<Character> challenges =  new GeneralTreeChallenges<>();
-        String expected =  "a,b,c,d,e,f,";
+        GeneralTreeChallenges<Character> challenges = new GeneralTreeChallenges<>();
+        String expected = "a,b,c,d,e,f,";
         Assert.assertEquals(challenges.bfsTraversal(root), expected);
     }
 
     @Test
-    public void testSearch(){
+    public void testSearch() {
         Node<Integer> root = new Node<>(10),
                 b = new Node<>(5),
                 c = new Node<>(8),
@@ -65,13 +65,74 @@ public class GeneralTreeChallengesTest {
         b.leftChild = d;
         b.rightChild = e;
 
-        c.rightChild =  f;
+        c.rightChild = f;
 
-        GeneralTreeChallenges<Integer> challenges =  new GeneralTreeChallenges<>();
+        GeneralTreeChallenges<Integer> challenges = new GeneralTreeChallenges<>();
         Assert.assertTrue(challenges.searchIterative(root, 11));
         Assert.assertFalse(challenges.searchIterative(root, 21));
 
         Assert.assertTrue(challenges.searchRecursive(root, 8));
         Assert.assertFalse(challenges.searchIterative(root, 1));
+    }
+
+    @Test
+    public void testGetNodesSum() {
+        Node<Integer> root = new Node<>(10),
+                b = new Node<>(5),
+                c = new Node<>(8),
+                d = new Node<>(4),
+                e = new Node<>(11),
+                f = new Node<>(6);
+
+        root.leftChild = b;
+        root.rightChild = c;
+
+        b.leftChild = d;
+        b.rightChild = e;
+
+        c.rightChild = f;
+        GeneralTreeChallenges<Integer> challenges = new GeneralTreeChallenges<>();
+        Assert.assertEquals(challenges.getNodesSumRecursive(root), 44);
+    }
+
+    @Test
+    public void testFindMinimumValue() {
+        Node<Integer> root = new Node<>(5),
+                b = new Node<>(11),
+                c = new Node<>(3),
+                d = new Node<>(4),
+                e = new Node<>(15),
+                f = new Node<>(12);
+
+        root.leftChild = b;
+        root.rightChild = c;
+
+        b.leftChild = d;
+        b.rightChild = e;
+
+        c.rightChild = f;
+        GeneralTreeChallenges<Integer> challenges = new GeneralTreeChallenges<>();
+        Assert.assertEquals(challenges.findMinimumValueRecursive(root), 3);
+        Assert.assertEquals(challenges.findMinimumValueIterative(root), 3);
+    }
+
+    @Test
+    public void testMaxRootToLeafPathSum() {
+        Node<Integer> root = new Node<>(5),
+                b = new Node<>(11),
+                c = new Node<>(3),
+                d = new Node<>(4),
+                e = new Node<>(2),
+                f = new Node<>(1);
+
+        root.leftChild = b;
+        root.rightChild = c;
+
+        b.leftChild = d;
+        b.rightChild = e;
+
+        c.rightChild = f;
+        GeneralTreeChallenges<Integer> challenges = new GeneralTreeChallenges<>();
+        Assert.assertEquals(challenges.maxRootToLeafPathSumRecursive(root), 20);
     }
 }
