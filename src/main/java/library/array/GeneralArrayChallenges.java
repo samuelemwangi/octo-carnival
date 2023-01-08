d86ee45b52faed20ced21c59d1033349e706a8a3
Samuel Mwangi <mwangi.s.irungu@gmail.com>
2023-01-08 14:16:11 +0300

    Array challenges - sliding window

--- src/main/java/library/array/GeneralArrayChallenges.java
+++ src/main/java/library/array/GeneralArrayChallenges.java
@@ -1,6 +1,8 @@
 package library.array;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class GeneralArrayChallenges {
 
@@ -54,4 +56,91 @@ public class GeneralArrayChallenges {
 
         return new int[]{-1, -1};
     }
+
+    /***
+     * @param arr subject array
+     * @param windowSize subject window size
+     * @return maximum sum
+     * @complexity Time -> O(nk), Space -> O(1)
+     */
+    public int maximumSumBruteForce(int[] arr, int windowSize) {
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i <= arr.length - windowSize; i++) {
+            int currentSum = 0;
+            for (int j = i; j < i + windowSize; j++) {
+                currentSum += arr[j];
+            }
+            maxSum = Math.max(currentSum, maxSum);
+        }
+
+        return maxSum;
+    }
+
+    /***
+     * @param arr  subject array
+     * @param windowSize subject window size
+     * @return maximum sum
+     * @complexity Time -> O(n), Space -> O(1)
+     */
+    public int maximumSumSlidingWindow(int[] arr, int windowSize) {
+        int maxSum = Integer.MIN_VALUE,
+                holdSum = 0,
+                j = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            holdSum += arr[i];
+            if (i - j == windowSize - 1) {
+                maxSum = Math.max(holdSum, maxSum);
+                holdSum -= arr[j++];
+            }
+        }
+
+        return maxSum;
+    }
+
+    /***
+     *
+     * @param arr subject array
+     * @param k window size
+     * @return list of maximum in windows
+     * @complexity Time -> O(n * k), Space -> O(1)
+     */
+    public List<Integer> maximumInSlidingWindowBruteForce(int[] arr, int k) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i <= arr.length - k; i++) {
+            int currMax = Integer.MIN_VALUE;
+            for (int j = i; j < i + k; j++) {
+                currMax = Math.max(currMax, arr[j]);
+            }
+            result.add(currMax);
+        }
+
+        return result;
+    }
+
+    /***
+     * @param arr subject array
+     * @param k subject window size
+     * @return list of maximums per window
+     * @complexity Time -> O(n), Space -> O(1)
+     */
+    public List<Integer> maximumInSlidingWindowOptimized(int[] arr, int k) {
+        List<Integer> result = new ArrayList<>();
+        int currMax = Integer.MIN_VALUE,
+                left = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            currMax = Math.max(currMax, arr[i]);
+            if (i - left == k - 1) {
+                result.add(currMax);
+                currMax = Integer.MIN_VALUE;
+                left++;
+            }
+        }
+
+        return result;
+    }
+
+
 }
--- src/test/java/library/array/GeneralArrayChallengesTest.java
+++ src/test/java/library/array/GeneralArrayChallengesTest.java
@@ -3,6 +3,8 @@ package library.array;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class GeneralArrayChallengesTest {
 
     @Test
@@ -29,5 +31,53 @@ public class GeneralArrayChallengesTest {
 
         Assert.assertEquals(twoSumExtraDSResult1[0] + twoSumExtraDSResult1[1], 10);
         Assert.assertTrue(twoSumExtraDSResult2[0] == -1 && twoSumExtraDSResult2[1] == -1);
+    }
+
+    @Test
+    public void testMaximumSum() {
+        int[] arr = {2, 1, 5, 1, 3, 2, 0, 6};
+        GeneralArrayChallenges challenges = new GeneralArrayChallenges();
+
+        Assert.assertEquals(challenges.maximumSumBruteForce(arr, 3), 9);
+        Assert.assertEquals(challenges.maximumSumBruteForce(arr, 1), 6);
+        Assert.assertEquals(challenges.maximumSumBruteForce(arr, 4), 11);
+
+        Assert.assertEquals(challenges.maximumSumSlidingWindow(arr, 3), 9);
+        Assert.assertEquals(challenges.maximumSumSlidingWindow(arr, 1), 6);
+        Assert.assertEquals(challenges.maximumSumSlidingWindow(arr, 4), 11);
+    }
+
+    @Test
+    public void testMaximumInSlidingWindow() {
+        int[] arr1 = {-4, 2, -5, 3, 6};
+        int k1 = 3;
+        int[] expectedRes1 = {2, 3, 6};
+
+        GeneralArrayChallenges challenges = new GeneralArrayChallenges();
+
+        List<Integer> actualRes1 = challenges.maximumInSlidingWindowBruteForce(arr1, k1);
+        List<Integer> actualRes2 = challenges.maximumInSlidingWindowOptimized(arr1, k1);
+
+        Assert.assertEquals(toIntArray(actualRes1), expectedRes1);
+        Assert.assertEquals(toIntArray(actualRes2), expectedRes1);
+    }
+
+    private int[] toIntArray(List<Integer> list) {
+        int[] res = new int[list.size()];
+
+        for (int i = 0; i < res.length; i++)
+            res[i] = list.get(i);
+
+        return res;
+    }
+
+    private boolean compareArrays(int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length)
+            return false;
+
+        for (int i = 0; i < arr1.length; i++)
+            if (arr1[i] != arr2[i])
+                return false;
+        return true;
     }
 }
