bd936518017fccf90aad92854c24e28986ed7228
Samuel Mwangi <mwangi.s.irungu@gmail.com>
2023-01-08 11:00:48 +0300

    Array challenges - two sum

--- src/main/java/library/array/GeneralArrayChallenges.java
+++ src/main/java/library/array/GeneralArrayChallenges.java
@@ -1,6 +1,14 @@
 package library.array;
 
+import java.util.HashSet;
+
 public class GeneralArrayChallenges {
+
+    /***
+     * @param arr array to search
+     * @return missing number
+     * @complexity Time -> O(n), Space -> O(1)
+     */
     public int missingNumberInGivenArray(int[] arr) {
         int sum = 0;
         for (int i = 1; i <= arr.length + 1; i++)
@@ -12,5 +20,38 @@ public class GeneralArrayChallenges {
             arrSum += j;
 
         return sum - arrSum;
+    }
+
+    /***
+     * @param arr array to search
+     * @param sum target sum
+     * @return the values
+     * @complexity Time -> O(n^2), Space -> O(1)
+     */
+    public int[] twoSumBruteForce(int[] arr, int sum) {
+        for (int i = 0; i < arr.length; i++)
+            for (int j = i + 1; j < arr.length; j++)
+                if (arr[i] + arr[j] == sum)
+                    return new int[]{arr[i], arr[j]};
+
+        return new int[]{-1, -1};
+    }
+
+    /***
+     * @param arr array to search
+     * @param sum target sum
+     * @return the values
+     * @complexity Time -> O(n), Space -> O(n)
+     */
+    public int[] twoSumExtraDS(int[] arr, int sum) {
+        HashSet<Integer> tracker = new HashSet<>();
+
+        for (int j : arr)
+            if (tracker.contains(sum - j))
+                return new int[]{j, sum - j};
+            else
+                tracker.add(j);
+
+        return new int[]{-1, -1};
     }
 }
--- src/test/java/library/array/GeneralArrayChallengesTest.java
+++ src/test/java/library/array/GeneralArrayChallengesTest.java
@@ -6,10 +6,28 @@ import org.testng.annotations.Test;
 public class GeneralArrayChallengesTest {
 
     @Test
-    public void testMissingNumberInGivenArray(){
-        int[] arr =  {3,7,1,2,8,4,5};
-        GeneralArrayChallenges challenges =  new GeneralArrayChallenges();
+    public void testMissingNumberInGivenArray() {
+        int[] arr = {3, 7, 1, 2, 8, 4, 5};
+        GeneralArrayChallenges challenges = new GeneralArrayChallenges();
 
         Assert.assertEquals(challenges.missingNumberInGivenArray(arr), 6);
+    }
+
+    @Test
+    public void testTwoSum() {
+        int[] arr = {5, 7, 1, 2, 8, 4, 3};
+        GeneralArrayChallenges challenges = new GeneralArrayChallenges();
+
+        int[] bruteForceResult1 = challenges.twoSumBruteForce(arr, 10);
+        int[] bruteForceResult2 = challenges.twoSumBruteForce(arr, 19);
+
+        Assert.assertEquals(bruteForceResult1[0] + bruteForceResult1[1], 10);
+        Assert.assertTrue(bruteForceResult2[0] == -1 && bruteForceResult2[1] == -1);
+
+        int[] twoSumExtraDSResult1 = challenges.twoSumExtraDS(arr, 10);
+        int[] twoSumExtraDSResult2 = challenges.twoSumExtraDS(arr, 19);
+
+        Assert.assertEquals(twoSumExtraDSResult1[0] + twoSumExtraDSResult1[1], 10);
+        Assert.assertTrue(twoSumExtraDSResult2[0] == -1 && twoSumExtraDSResult2[1] == -1);
     }
 }
